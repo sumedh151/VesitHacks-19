c88b8997bb9ea6286491698b7473433346224b9f
Vignesh Pillai <2017.vignesh.pillai@ves.ac.in>
2019-09-22 12:41:57 +0530

    Auto stash before merge of "master" and "origin/master"

--- vesithacks19/vesithacks19/urls.py
+++ vesithacks19/vesithacks19/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls import url
 
 urlpatterns = [
     path('',include('LandingPage.urls')),

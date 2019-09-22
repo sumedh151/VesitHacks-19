c463b174e591b04257ed9066aa1680a2380807a8
Harshita <h2010singh@gmail.com>
2019-09-22 18:26:08 +0530

    Dashboard Views

--- vesithacks19/LandingPage/views.py
+++ vesithacks19/LandingPage/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def test(request):
+    return render(request,'team_member/team_member_index.html')
+
+def history(request):
+    return render(request, "team_member/team_member_history.html")
+
+def edit(request):
+    return render(request, "team_member/edit_profile.html")

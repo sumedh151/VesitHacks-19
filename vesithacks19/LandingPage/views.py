7a9efa0b9c3d676122cb5895a6374a3db6d96adf
Jay <2017.jay.bendre@ves.ac.in>
2019-09-22 12:50:10 +0530

    Final Setup

--- vesithacks19/LandingPage/views.py
+++ vesithacks19/LandingPage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hi</h1><a href=/login/google-oauth2>Login</a>")
+    return render(request,'layout/index.html')
--- vesithacks19/vesithacks19/settings.py
+++ vesithacks19/vesithacks19/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'vesithacks19.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': os.path.join(BASE_DIR,'templates'),
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

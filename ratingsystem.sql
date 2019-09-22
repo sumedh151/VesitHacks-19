9b783792097ab8aa27663d6e1ec89e17e0f6d630
Sumedh Ghavat <ghavatsumedh@gmail.com>
2019-09-22 13:15:23 +0530

    ratingsystem.sql

---
+++ ratingsystem.sql
@@ -0,0 +1,154 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 22, 2019 at 09:45 AM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ratingsystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `changes`
+--
+
+CREATE TABLE `changes` (
+  `ssn` int(11) DEFAULT NULL,
+  `team_change` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `note_id` int(11) NOT NULL,
+  `ssn` int(11) DEFAULT NULL,
+  `initiated_at` datetime DEFAULT NULL,
+  `type` int(1) DEFAULT NULL,
+  `content` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `team_id` int(11) NOT NULL,
+  `task_id` int(11) NOT NULL,
+  `ssn` int(11) NOT NULL,
+  `deadline` text,
+  `rating` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `team`
+--
+
+CREATE TABLE `team` (
+  `t_id` int(11) NOT NULL,
+  `team_name` varchar(30) DEFAULT NULL,
+  `mgr_ssn` int(11) DEFAULT NULL,
+  `team_path` text,
+  `members` text,
+  `team_status` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `ssn` int(11) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `t_id` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `changes`
+--
+ALTER TABLE `changes`
+  ADD KEY `ssn` (`ssn`);
+
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`note_id`),
+  ADD KEY `ssn` (`ssn`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`team_id`,`task_id`,`ssn`);
+
+--
+-- Indexes for table `team`
+--
+ALTER TABLE `team`
+  ADD PRIMARY KEY (`t_id`),
+  ADD KEY `mgr_ssn` (`mgr_ssn`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`ssn`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `changes`
+--
+ALTER TABLE `changes`
+  ADD CONSTRAINT `changes_ibfk_1` FOREIGN KEY (`ssn`) REFERENCES `user` (`ssn`);
+
+--
+-- Constraints for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`ssn`) REFERENCES `user` (`ssn`);
+
+--
+-- Constraints for table `team`
+--
+ALTER TABLE `team`
+  ADD CONSTRAINT `team_ibfk_1` FOREIGN KEY (`mgr_ssn`) REFERENCES `user` (`ssn`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

c88a4c303ad014a554ea22a313043969ff990321
Harshita <h2010singh@gmail.com>
2019-09-23 08:22:57 +0530

    na

--- vesithacks19/LandingPage/urls.py
+++ vesithacks19/LandingPage/urls.py
@@ -14,8 +14,8 @@ urlpatterns=[
     path('log_out',views.log_out,name='logout'),
     path('submit',views.check_if_submitted,name="check_if_submitted"),
 
-    url(r'^test/$', views.test, name="test"),
-    url(r'^history/$', views.history, name="history"),
+    #url(r'^test/$', views.test, name="test"),
+    #url(r'^history/$', views.history, name="history"),
 
     #url(r'^test/$', views.test, name="test"),
     url(r'^history/$', views.team_member_history, name="history"),
